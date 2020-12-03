28578758f908de6f584580a957a26c0639bdcf0c
CundullahT <cundullahterzioglu.it@gmail.com>
2020-12-03 03:09:57 +0100

    MyLinkedList Update.

---
+++ DataStructures-06-Trees/src/Tree.java
@@ -0,0 +1,99 @@
+public class Tree {
+
+    // Nested Node Class
+    private class Node {
+
+        private Node leftChild;
+        private Node rightChild;
+        private int value;
+
+        public Node(int value) {
+
+            this.value = value;
+
+        }
+
+    }
+
+    private Node root;
+
+    // My Insert Method for BST
+    public void insert(int value) {
+
+        Node node = new Node(value);
+
+        // If tree is empty
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        // If tree is not empty
+        Node current = root;
+
+        while (true) {
+            // Check if less
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+
+    }
+
+    // Pre-Order -> Root - Left - Right
+    public void traversePreOrder(){
+        traversePreOrder(root);
+    }
+    private void traversePreOrder(Node root) {
+        if(root == null) return;
+        System.out.print(root.value + ", ");
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+    // In-Order ->  Left - Root - Right
+    public void traverseInOrder(){
+        traverseInOrder(root);
+    }
+    private void traverseInOrder(Node root) {
+        if(root == null) return;
+        traverseInOrder(root.leftChild);
+        System.out.print(root.value + ", ");
+        traverseInOrder(root.rightChild);
+    }
+    // Post-Order -> Left - Right - Root
+    public void traversePostOrder(){
+        traversePostOrder(root);
+    }
+    private void traversePostOrder(Node root) {
+        if(root == null) return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.print(root.value + ", ");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
