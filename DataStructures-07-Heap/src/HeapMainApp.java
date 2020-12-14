cf143f92d2abc59dd41ce27445b749c5dd355eb0
CundullahT <cundullahterzioglu.it@gmail.com>
2020-12-14 02:03:14 +0100

    MyLinkedList Update.

---
+++ DataStructures-07-Heap/src/HeapMainApp.java
@@ -0,0 +1,18 @@
+public class HeapMainApp {
+
+    public static void main(String[] args) {
+
+        MyHeap myHeap = new MyHeap();
+
+        myHeap.insert(23);
+        myHeap.insert(32);
+        myHeap.insert(91);
+        myHeap.insert(100);
+        myHeap.insert(2);
+        myHeap.insert(100);
+
+        myHeap.print();
+
+    }
+
+}
